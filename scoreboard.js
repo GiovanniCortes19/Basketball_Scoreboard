f1e69b75867d969605ccfe54f045bd524bfe4667
GiovanniCortes <giocortes19@gmail.com>
2023-02-23 16:52:56 -0400

    added feature to highlight which team is winning

--- scoreboard.js
+++ scoreboard.js
@@ -3,15 +3,21 @@ let homeScore = document.getElementById("home-score");
 let guestScore = document.getElementById("guest-score");
 let homePoints = 0;
 let guestPoints = 0;
+
+let homeBox = document.getElementById("home-box");
+let guestBox = document.getElementById("guest-box");
+
 //Functions
 function homeAddPoints(n) {
   homePoints += n;
   homeScore.textContent = homePoints;
+  isWinning();
 }
 
 function guestAddPoints(n) {
   guestPoints += n;
   guestScore.textContent = guestPoints;
+  isWinning();
 }
 
 function newGame() {
@@ -19,4 +25,19 @@ function newGame() {
   guestPoints = 0;
   homeScore.textContent = homePoints;
   guestScore.textContent = guestPoints;
+  guestBox.style.borderColor = "black";
+  homeBox.style.borderColor = "black";
+}
+
+function isWinning() {
+  if (homePoints > guestPoints) {
+    homeBox.style.borderColor = "goldenrod";
+    guestBox.style.borderColor = "black";
+  } else if (homePoints < guestPoints) {
+    guestBox.style.borderColor = "goldenrod";
+    homeBox.style.borderColor = "black";
+  } else {
+    guestBox.style.borderColor = "black";
+    homeBox.style.borderColor = "black";
+  }
 }
